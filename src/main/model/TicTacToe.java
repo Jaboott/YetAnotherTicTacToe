32d4ded2675cda9140eb2c5a4c61e79842d7f3e4
Jason <jxu60@students.cs.ubc.ca>
2023-02-25 14:47:03 -0800

    made 2 functions and the constructor -printBoard -changeBoard

---
+++ src/main/model/TicTacToe.java
@@ -0,0 +1,51 @@
+package model;
+
+public class TicTacToe {
+
+    private static final int ROW = 3;
+    private static final int COLUMN = 3;
+
+    private String[][] board;
+
+
+    // MODIFY: this
+    // EFFECTS: creates a 2d array with numbers 0-8 filled in each index
+    public TicTacToe() {
+        board = new String[ROW][COLUMN];
+    }
+
+    // MODIFY: this
+    // EFFECT: prints the 2d array with spaces in between each index
+    //         prints number between 0-8 if slot is not taken
+    public void printBoard() {
+        int count = 0;
+        System.out.println("\n\n\n\n\n\n\n\n\n\n");
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMN; j++) {
+                if (board[i][j] == null) {
+                    System.out.print(count + "     ");
+                } else {
+                    System.out.print(board[i][j] + "     ");
+                }
+                count++;
+            }
+            System.out.println("\n");
+        }
+    }
+
+    // REQUIRE: index is between 0 - 8, value is either "X" or "O"
+    // MODIFY: this
+    // EFFECT: sets a specified index on the 2d array to a specific value
+    //         if the given slot is empty
+    public void changeBoard(int index, String value) {
+        if (board[index / ROW][(index % COLUMN)] == null) {
+            board[index / ROW][(index % COLUMN)] = value;
+        } else {
+            System.out.println("The given slot is already taken!");
+        }
+    }
+
+
+
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,16 @@
 package ui;
 
+import model.TicTacToe;
+
 public class Main {
     public static void main(String[] args) {
-
+        TicTacToe game = new TicTacToe();
+        game.printBoard();
+        game.changeBoard(6, "X");
+        game.printBoard();
+        game.changeBoard(6, "O");
+        game.printBoard();
+        game.changeBoard(7, "O");
+        game.printBoard();
     }
 }
