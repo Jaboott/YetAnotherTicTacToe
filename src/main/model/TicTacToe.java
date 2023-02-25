2be0bfe78948a6baeea74a1d8401b74ec65360ba
Jason <jxu60@students.cs.ubc.ca>
2023-02-25 15:45:36 -0800

    made the function checkDiagonal and initializeBoard

--- src/main/model/TicTacToe.java
+++ src/main/model/TicTacToe.java
@@ -12,6 +12,17 @@ public class TicTacToe {
     // EFFECTS: creates a 2d array with numbers 0-8 filled in each index
     public TicTacToe() {
         board = new String[ROW][COLUMN];
+        initializeBoard();
+    }
+
+    // MODIFY: this
+    // EFFECTS: sets every index of the 2d array to empty string
+    private void initializeBoard() {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COLUMN; j++) {
+                board[i][j] = "";
+            }
+        }
     }
 
     // MODIFY: this
@@ -22,7 +33,7 @@ public class TicTacToe {
         System.out.println("\n\n\n\n\n\n\n\n\n\n");
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUMN; j++) {
-                if (board[i][j] == null) {
+                if (board[i][j].equals("")) {
                     System.out.print(count + "     ");
                 } else {
                     System.out.print(board[i][j] + "     ");
@@ -38,38 +49,57 @@ public class TicTacToe {
     // EFFECT: sets a specified index on the 2d array to a specific value
     //         if the given slot is empty
     public void changeBoard(int index, String value) {
-        if (board[index / ROW][(index % COLUMN)] == null) {
+        if (board[index / ROW][(index % COLUMN)].equals("")) {
             board[index / ROW][(index % COLUMN)] = value;
         } else {
             System.out.println("The given slot is already taken!");
         }
     }
 
+    // EFFECT: checks the status of the game
+    //         prints "Rows" if a row is completed
+    //         prints "Columns" if a column is completed
+    //         prints "Diagonal" if a diagonal is completed
+    //         prints "game have not ended"" if the game have not ended
     public void checkStatus() {
         if (checkRows()) {
             System.out.println("Rows");
-        }
-        if (checkColumns()) {
+        } else if (checkColumns()) {
             System.out.println("Columns");
+        } else if (checkDiagonal()) {
+            System.out.println("Diagonal");
+        } else {
+            System.out.println("game have not ended");
         }
+
     }
 
+    // EFFECTS: returns true if a row is completed return false otherwise
     private boolean checkRows() {
         for (int i = 0; i < ROW; i++) {
-            if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
+            if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][1].equals("")) {
                 return true;
             }
         }
         return false;
     }
 
+    // EFFECTS: returns true if a column is completed return false otherwise
     private boolean checkColumns() {
         for (int i = 0; i < COLUMN; i++) {
-            if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
+            if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[1][i].equals("")) {
                 return true;
             }
         }
         return false;
+    }
+
+    // EFFECTS: returns true if a diagonal is completed return false otherwise
+    private boolean checkDiagonal() {
+        boolean result =
+                (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[1][1].equals(""))
+                || (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[1][1].equals(""));
+        return result;
     }
 
 
