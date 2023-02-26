0eda349713c01e79a66e1e5497858635f23bf010
Jason <jxu60@students.cs.ubc.ca>
2023-02-26 15:45:45 -0800

    Added documentation and made the functions finalMessage and checkEnd

--- src/main/ui/TicTacToeApp.java
+++ src/main/ui/TicTacToeApp.java
@@ -16,10 +16,11 @@ public class TicTacToeApp {
         runTicTacToe();
     }
 
+    // MODIFY: this
+    // EFFECTS: runs the game
     public void runTicTacToe() {
         int currPlayer = chooseFirstPlayer();
         String symbol = "";
-        Scanner input = new Scanner(System.in);
         boolean gameRunning = true;
 
         init();
@@ -33,23 +34,27 @@ public class TicTacToeApp {
 
             if (checkInput(currMove)) {
                 game.changeBoard(currMove, symbol);
+                game.printBoard();
+                finalMessage(currPlayer);
+                gameRunning = checkEnd();
+                currPlayer = changePlayer(currPlayer);
             }
-
-            game.printBoard();
-            currPlayer = changePlayer(currPlayer);
 
         }
     }
 
-
+    // MODIFY: this
+    // EFFECTS: makes a new tic-tac-toe game
     private void init() {
         game = new TicTacToe();
     }
 
+    // EFFECTS: prints out instructions
     private void printInstructions(int currentPlayer) {
         System.out.println("Player " + currentPlayer + " pls input a number between 0 to 8");
     }
 
+    // EFFECTS: choose a random player
     private int chooseFirstPlayer() {
         Random r = new Random();
         int num1 = 0;
@@ -67,6 +72,7 @@ public class TicTacToeApp {
 
     }
 
+    // EFFECTS: prints message and return false if input is not valid, return true otherwise
     private boolean checkInput(int input) {
         if (input > 8 || input < 0 || game.slotTaken(input)) {
             System.out.println("Input not in range or taken pls try again");
@@ -75,6 +81,7 @@ public class TicTacToeApp {
         return true;
     }
 
+    // EFFECTS: takes user input until user inputs a valid input
     private int inputNextMove() {
         int playerInput = 0;
         Scanner input = new Scanner(System.in);
@@ -88,6 +95,7 @@ public class TicTacToeApp {
         return playerInput;
     }
 
+    // EFFECTS: returns "X" or "O" depending on the inputted player
     private String changeSymbol(int player) {
         if (player == 1) {
             return PLAYER1;
@@ -95,11 +103,37 @@ public class TicTacToeApp {
         return PLAYER2;
     }
 
+    // EFFECTS: change the given player to the next player
     private int changePlayer(int player) {
         if (player == 1) {
             return 2;
         }
         return 1;
+    }
+
+    // REQUIRE: currPlayer needs to be either 1 or 2
+    // EFFECTS: outputs message for given game status
+    private void finalMessage(int currPlayer) {
+        String message = game.checkStatus();
+
+        if (message.equals("It is a tie")) {
+            System.out.println("The players tied, no winner");
+        } else if (message.equals("")) {
+            System.out.println("");
+        } else {
+            System.out.println("Player" + currPlayer + " have won by " + message);
+        }
+    }
+
+    // EFFECTS: returns true if game have not ended, false if game have ended
+    private boolean checkEnd() {
+        String message = game.checkStatus();
+
+        if (message.equals("")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
